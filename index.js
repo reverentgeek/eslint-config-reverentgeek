04f2fd09c869dbdad262aec20f4a6af2c459cbc7
David Neal <david@reverentgeek.com>
2019-12-13 09:52:43 -0500

    adds okta config

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es6: true,
-    node: true
-  },
-  extends: "eslint:recommended",
-  globals: {},
-  parserOptions: {
-    ecmaVersion: 2019
-  },
-  rules: {
-    indent: [ "error", 2 ],
-    "linebreak-style": [ "error", "unix" ],
-    quotes: [ "error", "double" ],
-    semi: [ "error", "always" ],
-    "array-bracket-spacing": [ "error", "always" ],
-    "object-curly-spacing": [ "error", "always" ],
-    "space-in-parens": [ "error", "always" ]
-  }
+	env: {
+		commonjs: true,
+		es6: true,
+		node: true
+	},
+	extends: "eslint:recommended",
+	globals: {},
+	parserOptions: {
+		ecmaVersion: 2019
+	},
+	rules: {
+		indent: [ "error", "tab" ],
+		"linebreak-style": [ "error", "unix" ],
+		quotes: [ "error", "double" ],
+		semi: [ "error", "always" ],
+		"array-bracket-spacing": [ "error", "always" ],
+		"object-curly-spacing": [ "error", "always" ],
+		"space-in-parens": [ "error", "always" ]
+	}
 };
---
+++ okta.js
@@ -0,0 +1,6 @@
+module.exports = {
+	extends: "./index.js",
+	rules: {
+		indent: [ "error", 2 ]
+	}
+};
