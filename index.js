4bf6e8dfefdbd5115460248843cff509440a05d9
David Neal <david@reverentgeek.com>
2020-04-22 11:45:29 -0400

    add no-var rule

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ module.exports = {
 		"no-console": [ "off" ],
 		"no-multiple-empty-lines": [ "error", { max: 1 } ],
 		"no-trailing-spaces": [ "error", { skipBlankLines: false, ignoreComments: false } ],
+		"no-var": [ "error" ],
 		"object-curly-spacing": [ "error", "always" ],
 		quotes: [ "error", "double" ],
 		semi: [ "error", "always" ],
